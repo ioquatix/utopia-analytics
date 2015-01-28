5fa0087b23d0cdee341d8bc29e0800e5f03f2364
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2015-01-28 13:33:07 +1300

    Bump version.

--- lib/utopia/tags/google-analytics.rb
+++ lib/utopia/tags/google-analytics.rb
@@ -18,27 +18,26 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'utopia/tags'
-
 module Utopia
 	module Tags
 		module GoogleAnalytics
 			def self.call(transaction, state)
+				id = state['id']
+				
 				html = <<-EOF
-					<script type="text/javascript">
-						var _gaq = _gaq || []; _gaq.push(['_setAccount', #{state[:id].to_quoted_string}]); _gaq.push(['_trackPageview']);
-						(function() {
-							var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-							ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-							var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-						})();
+					<script>
+						(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+							(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+							m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+						})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+							
+						ga('create', #{Trenni::Strings.to_quoted_string(id)}, 'auto');
+						ga('send', 'pageview');
 					</script>
 				EOF
-
+				
 				transaction.cdata(html)
 			end
 		end
 	end
 end
-
-Utopia::Tags.register("google-analytics", Utopia::Tags::GoogleAnalytics)
--- utopia-tags-google-analytics.gemspec
+++ utopia-tags-google-analytics.gemspec
@@ -29,5 +29,5 @@ Gem::Specification.new do |spec|
 	spec.add_development_dependency "bundler", "~> 1.3"
 	spec.add_development_dependency "rake"
 	
-	spec.add_dependency "utopia", "~> 0.12.0"
+	spec.add_dependency "utopia", "~> 1.0"
 end
